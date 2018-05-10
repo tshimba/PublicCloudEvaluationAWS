58e02b2e7fd67fd233dad555b708be317ce7605a
shimba <tshimba@jp.ibm.com>
2018-05-10 14:28:57 +0900

    first commit

---
+++ src/main/java/com/taikishimba/aws/PublicCloudEvaluationAWS/PublicCloudEvaluationAwsApplication.java
@@ -0,0 +1,12 @@
+package com.taikishimba.aws.PublicCloudEvaluationAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicCloudEvaluationAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicCloudEvaluationAwsApplication.class, args);
+	}
+}
